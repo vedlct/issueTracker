8170731a66b09113f9f89adcdede0bf3af8b3985
Arabi Kabir <mail2arabi@gmail.com>
2019-01-17 16:41:52 +0600

    ticket view updated

--- resources/views/Ticket/ticketDetails.blade.php
+++ resources/views/Ticket/ticketDetails.blade.php
@@ -1,12 +1,63 @@
 @extends('layouts.mainLayout')
 
-{{--@section('css')--}}
+@section('css')
     {{--<style >--}}
         {{--.table-condensed>thead>tr>th, .table-condensed>tbody>tr>th, .table-condensed>tfoot>tr>th, .table-condensed>thead>tr>td, .table-condensed>tbody>tr>td, .table-condensed>tfoot>tr>td{--}}
             {{--padding: 1px;--}}
         {{--}--}}
     {{--</style>--}}
-{{--@endsection--}}
+    <style>
+        .container2 {
+            border: 2px solid #dedede;
+            background-color: #f1f1f1;
+            border-radius: 5px;
+            padding: 10px;
+            margin: 10px 0;
+        }
+
+        /* Darker chat container */
+        .darker {
+            border-color: #ccc;
+            background-color: #ddd;
+        }
+
+        /* Clear floats */
+        .container2::after {
+            content: "";
+            clear: both;
+            display: table;
+        }
+
+        /* Style images */
+        .container2 img {
+            float: left;
+            max-width: 60px;
+            width: 100%;
+            margin-right: 20px;
+            border-radius: 50%;
+        }
+
+        /* Style the right image */
+        .container2 img.right {
+            float: right;
+            margin-left: 20px;
+            margin-right:0;
+        }
+
+        /* Style time text */
+        .time-right {
+            float: right;
+            color: #aaa;
+        }
+
+        /* Style time text */
+        .time-left {
+            float: left;
+            color: #999;
+        }
+    </style>
+    <link href="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.11/summernote.css" rel="stylesheet">
+@endsection
 
 @section('content')
 
@@ -82,11 +133,23 @@
                 <h4>Ticket Discussion</h4>
             </div>
             <div class="card-body">
+                {{-- Old reply --}}
+                <div class="container2">
+                    <img src="{{ asset('public/images/avatar/1.png') }}" alt="Avatar">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda delectus, dicta doloremque ea eligendi libero sequi similique. A accusamus architecto asperiores aspernatur dicta, eos error eum eveniet fuga ipsa ipsam iste non odit pariatur quam quidem reprehenderit, sapiente unde. At beatae culpa, dignissimos dolores exercitationem id inventore laudantium necessitatibus nostrum omnis perspiciatis possimus praesentium quae quas quibusdam quisquam sunt totam. Architecto aspernatur commodi, eos esse excepturi inventore non ratione suscipit totam voluptates. Officiis, quis voluptas!</p>
+                    <span class="time-right">11:00</span>
+                </div>
+
+                <div class="container2 darker">
+                    <img src="{{ asset('public/images/avatar/2.png') }}" alt="Avatar" class="right">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi at culpa distinctio exercitationem, explicabo magni minus molestias perspiciatis quae saepe. At inventore labore non. Assumenda at autem culpa cupiditate, earum eius id illo ipsam libero maiores modi nisi, nostrum officiis perspiciatis quaerat quis reprehenderit similique sint soluta vitae. Debitis, provident!</p>
+                    <span class="time-left">11:01</span>
+                </div>
 
                 {{-- Post a reply --}}
                 <form>
                     <div class="form-group">
-                        <textarea class="form-control" id="exampleFormControlTextarea1" placeholder="Enter Your reply" rows="3"></textarea>
+                        <textarea id="summernote" name="editordata" class="form-control" placeholder="Enter Your reply" rows="3"></textarea>
                         <button type="submit" class="btn btn-primary float-right mt-3">Post Reply</button>
                     </div>
                 </form>
@@ -97,8 +160,15 @@
 @endsection
 
 @section('js')
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.11/summernote.js"></script>
 
     <script>
+
+        // $(document).ready(function() {
+        //     $('#summernote').summernote({
+        //         focus: false
+        //     });
+        // });
 
         {{--$(document).ready(function() {--}}
 
