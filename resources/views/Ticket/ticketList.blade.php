b5314b97b829ede11d36ad49f4a3a48ef9515599
Arabi Kabir <mail2arabi@gmail.com>
2019-02-07 15:52:01 +0600

    new filter added

--- resources/views/Ticket/ticketList.blade.php
+++ resources/views/Ticket/ticketList.blade.php
@@ -173,25 +173,25 @@
 
 </div>
 
-{{--<!-- Edit Ticket Modal-->--}}
-{{--<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">--}}
-    {{--<div class="modal-dialog modal-lg" role="document">--}}
-        {{--<div class="modal-content ">--}}
-            {{--<div class="modal-header">--}}
-                {{--<h5 class="modal-title" id="exampleModalLabel">Edit Ticket</h5>--}}
-                {{--<button type="hidden" class="close" data-dismiss="modal" aria-label="Close">--}}
-                    {{--<span aria-hidden="true">&times;</span>--}}
-                {{--</button>--}}
-            {{--</div>--}}
-            {{--<div class="modal-body" id="editTicket">--}}
+<!-- Edit Ticket Modal-->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content ">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Edit Ticket</h5>
+                <button type="hidden" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body" id="editTicket">
 
-            {{--</div>--}}
-            {{--<div class="modal-footer">--}}
-                {{--<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>--}}
-            {{--</div>--}}
-        {{--</div>--}}
-    {{--</div>--}}
-{{--</div>--}}
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
 
 <!-- Assign Team to ticket-->
 <div class="modal fade" id="teamModal" role="dialog" aria-hidden="true">
